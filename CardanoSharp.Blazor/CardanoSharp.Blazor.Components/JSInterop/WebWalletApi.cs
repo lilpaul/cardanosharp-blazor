0778f2be1cfdd0cd5b71662bab4e6055c788c910
Paul Cowan <paul.cowan@gmail.com>
2022-06-28 19:56:09 +0200

    First draft of interop objects

---
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/JSInterop/WebWalletApi.cs
@@ -0,0 +1,69 @@
+﻿using CardanoSharp.Blazor.Components.Interfaces;
+using CardanoSharp.Blazor.Components.Models;
+using Microsoft.JSInterop;
+
+namespace CardanoSharp.Blazor.Components.JSInterop
+{
+	public class WebWalletApi : IWebWalletApi
+	{
+		private IJSObjectReference _jsWalletObj;
+
+		public WebWalletApi(IJSObjectReference jsWalletObj)
+		{
+			if (jsWalletObj == null)
+				throw new ArgumentNullException(nameof(jsWalletObj));
+
+			_jsWalletObj = jsWalletObj;
+		}
+
+		public async ValueTask<int> GetNetworkId()
+		{
+			return await _jsWalletObj.InvokeAsync<int>($"getNetworkId");
+		}
+
+		public async ValueTask<string[]> GetUtxos(string? amount = null, Paginate? paginate = null)
+		{
+			return await _jsWalletObj.InvokeAsync<string[]>($"getUtxos", amount, paginate);
+		}
+
+		public async ValueTask<string[]> GetCollateral(CollateralParams collateralParams)
+		{
+			return await _jsWalletObj.InvokeAsync<string[]>($"getCollateral", collateralParams);
+		}
+
+		public async ValueTask<string> GetBalance()
+		{
+			return await _jsWalletObj.InvokeAsync<string>($"getBalance");
+		}
+
+		public async ValueTask<string[]> GetUsedAddresses(Paginate? paginate = null)
+		{
+			return await _jsWalletObj.InvokeAsync<string[]>($"getUsedAddresses", paginate);
+		}
+
+		public async ValueTask<string> GetChangeAddress()
+		{
+			return await _jsWalletObj.InvokeAsync<string>($"getChangeAddress");
+		}
+
+		public async ValueTask<string[]> GetRewardAddresses()
+		{
+			return await _jsWalletObj.InvokeAsync<string[]>($"getRewardAddresses");
+		}
+
+		public async ValueTask<string> SignTx(string tx, bool partialSign = false)
+		{
+			return await _jsWalletObj.InvokeAsync<string>($"signTx", tx, partialSign);
+		}
+
+		public async ValueTask<DataSignature> SignData(string addr, string payload)
+		{
+			return await _jsWalletObj.InvokeAsync<DataSignature>($"signData", addr, payload);
+		}
+
+		public async ValueTask<string> SubmitTx(string tx)
+		{
+			return await _jsWalletObj.InvokeAsync<string>($"submitTx", tx);
+		}
+	}
+}
---
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/JSInterop/WebWalletInitialApi.cs
@@ -0,0 +1,47 @@
+﻿using CardanoSharp.Blazor.Components.Interfaces;
+using Microsoft.JSInterop;
+
+namespace CardanoSharp.Blazor.Components.JSInterop
+{
+	public class WebWalletInitialApi : IWebWalletInitialApi
+	{
+		private IJSRuntime _js;
+		private string _webWalletName;
+
+		public WebWalletInitialApi(IJSRuntime js, string webWalletName)
+		{
+			if (js == null)
+				throw new ArgumentNullException(nameof(js));
+			if (webWalletName == null || webWalletName.Length == 0)
+				throw new ArgumentNullException(nameof(webWalletName));
+
+			_js = js;
+			_webWalletName = webWalletName;
+		}
+
+		public async ValueTask<IJSObjectReference> Enable()
+		{
+			return await _js.InvokeAsync<IJSObjectReference>($"cardano.{_webWalletName}.enable");
+		}
+
+		public async ValueTask<bool> IsEnabled()
+		{
+			return await _js.InvokeAsync<bool>($"cardano.{_webWalletName}.isEnabled");
+		}
+
+		public async ValueTask<string> ApiVersion()
+		{
+			return await _js.InvokeAsync<string>($"cardano.{_webWalletName}.apiVersion");
+		}
+
+		public async ValueTask<string> Name()
+		{
+			return await _js.InvokeAsync<string>($"cardano.{_webWalletName}.name");
+		}
+
+		public async ValueTask<string> Icon()
+		{
+			return await _js.InvokeAsync<string>($"cardano.{_webWalletName}.icon");
+		}
+	}
+}
