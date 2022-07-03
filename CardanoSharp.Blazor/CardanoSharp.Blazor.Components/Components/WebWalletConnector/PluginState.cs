e393b45fd02c2967029787d79f865c2abda4f673
Paul Cowan <paul.cowan@gmail.com>
2022-07-03 15:40:17 +0200

    Copied across main structure of web wallet connector elements

--- CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Components/WebWalletConnector/PluginState.cs
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Components/WebWalletConnector/PluginState.cs
@@ -5,39 +5,39 @@ using Microsoft.JSInterop;
 
 namespace CardanoSharp.Blazor.Components
 {
-    public class PluginState : WalletExtension
-    {
-        public bool Installed { get; }
+	public class PluginState : WalletExtension
+	{
+		public bool Installed { get; }
 
-        public IWebWalletInitialApi InitialApi { get; }
+		public IWebWalletInitialApi InitialApi { get; }
 
-        public PluginState(WalletExtension extension, bool installed, IWebWalletInitialApi initialApi) : base(extension)
-        {
-            if (extension == null) throw new ArgumentNullException(nameof(extension));
-            if (initialApi == null) throw new ArgumentNullException(nameof(initialApi));
+		public PluginState(WalletExtension extension, bool installed, IWebWalletInitialApi initialApi) : base(extension)
+		{
+			if (extension == null) throw new ArgumentNullException(nameof(extension));
+			if (initialApi == null) throw new ArgumentNullException(nameof(initialApi));
 
-            Installed = installed;
-            InitialApi = initialApi;
-        }
+			Installed = installed;
+			InitialApi = initialApi;
+		}
 
-        public async ValueTask<WalletState?> ConnectAsync()
-        {
-            if (!Installed) return null;
-            try
-            {
-                var apiJsObj = await InitialApi.Enable().ConfigureAwait(false);
-                if (apiJsObj == null) return null;
-                var api = new WebWalletApi(apiJsObj);
-                var balanceCbor = await api.GetBalance().ConfigureAwait(false);
-                if (balanceCbor == null || balanceCbor.Length < 2) return null;
-                var changeAddress = await api.GetChangeAddress().ConfigureAwait(false);
-                if (changeAddress == null || changeAddress.Length < 10) return null;
-                return new WalletState(this, api, balanceCbor, changeAddress);
-            }
-            catch (JSException ex)
-            {
-                return null;
-            }
-        }
-    }
+		public async ValueTask<WalletState?> ConnectAsync()
+		{
+			if (!Installed) return null;
+			try
+			{
+				var apiJsObj = await InitialApi.Enable().ConfigureAwait(false);
+				if (apiJsObj == null) return null;
+				var api = new WebWalletApi(apiJsObj);
+				var wallet = new WalletState(this, api);
+				await wallet.InitAsync();
+				return wallet;
+			}
+			catch (JSException ex)
+			{
+				//catch and return null as user clicking cancel in wallet plugin can result exception
+				//depending on wallet implementation
+				return null;
+			}
+		}
+	}
 }
--- CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Components/WebWalletConnector/WalletState.cs
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Components/WebWalletConnector/WalletState.cs
@@ -8,84 +8,114 @@ using System.Globalization;
 
 namespace CardanoSharp.Blazor.Components
 {
-    public class WalletState : WalletExtension
-    {
-        public IWebWalletApi Api { get; }
+	public class WalletState : WalletExtension
+	{
+		public IWebWalletApi Api { get; }
 
-        public NetworkType Network { get; }
+		public NetworkType Network { get; }
 
-        public string ChangeAddress { get; } = "";
+		public string ChangeAddress { get; private set; } = "";
 
-        public ulong Balance { get; }
+		public ulong Balance { get; private set; }
 
-        public ulong TokenPreservation { get; }
+		public ulong TokenPreservation { get; private set; }
 
-        public List<Asset> Assets { get; } = new List<Asset>();
+		public List<Asset> Assets { get; private set; } = new List<Asset>();
 
-        public WalletState(WalletExtension walletExtension, IWebWalletApi api, string balanceCbor, string changeAddress)
-        {
-            if (walletExtension == null) throw new ArgumentNullException(nameof(walletExtension));
-            if (api == null) throw new ArgumentNullException(nameof(api));
-            if (balanceCbor == null) throw new ArgumentNullException(nameof(balanceCbor));
-            if (balanceCbor.Length < 2) throw new ArgumentException(nameof(balanceCbor), "must be at least 2 characters long");
-            if (changeAddress == null) throw new ArgumentNullException(nameof(changeAddress));
-            if (changeAddress.Length < 10) throw new ArgumentException(nameof(changeAddress), "must be at least 10 characters long");
+		public bool Initialized
+		{
+			get
+			{
+				return !string.IsNullOrEmpty(ChangeAddress);
+			}
+		}
 
-            Api = api;
-            ChangeAddress = changeAddress;
-            var balanceValue = balanceCbor.HexToByteArray().DeserializeTransactionOutputValue();
-            TokenPreservation = balanceValue.MultiAsset.CalculateMinUtxoLovelace();
-            Balance = balanceValue.Coin;
-        }
+		public WalletState(WalletExtension walletExtension, IWebWalletApi api)
+		{
+			if (walletExtension == null) throw new ArgumentNullException(nameof(walletExtension));
+			if (api == null) throw new ArgumentNullException(nameof(api));
+			Api = api;
+		}
 
-        public string BalanceAdaPortion
-        {
-            get
-            {
-                var temp = (Balance / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
-                return temp.Substring(0, temp.IndexOf('.'));
-            }
-        }
+		public async ValueTask InitAsync()
+		{
+			ChangeAddress = await Api.GetChangeAddress().ConfigureAwait(false);
+			await RefreshBalanceAsync();
+			return;
+		}
 
-        public string BalanceLovelacePortion
-        {
-            get
-            {
-                return (Balance % 1000000).ToString("D6");
-            }
-        }
+		public async ValueTask RefreshBalanceAsync()
+		{
+			var balanceCbor = await Api.GetBalance().ConfigureAwait(false);
+			var balanceValue = balanceCbor.HexToByteArray().DeserializeTransactionOutputValue();
+			TokenPreservation = balanceValue.MultiAsset.CalculateMinUtxoLovelace();
+			Balance = balanceValue.Coin;
+			Assets.Clear();
+			if (balanceValue.MultiAsset != null && balanceValue.MultiAsset.Count > 0)
+			{
+				balanceValue.MultiAsset.SelectMany(p => p.Value.Token.Select(a => new Asset()
+				{
+					PolicyId = p.Key.ToStringHex(),
+					Name = a.Key.ToString(),
+					Quantity = a.Value
+				})).ToList();
+			}
+		}
 
-        public string TokenPreservationAdaPortion
-        {
-            get
-            {
-                var temp = (TokenPreservation / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
-                return temp.Substring(0, temp.IndexOf('.'));
-            }
-        }
+		public async ValueTask<bool> ConnectedWalletChanged()
+		{
+			var apiChangeAddress = await Api.GetChangeAddress().ConfigureAwait(false);
+			return !String.Equals(apiChangeAddress, ChangeAddress, StringComparison.OrdinalIgnoreCase);
+		}
 
-        public string TokenPreservationLovelacePortion
-        {
-            get
-            {
-                return (TokenPreservation % 1000000).ToString("D6");
-            }
-        }
+		public string BalanceAdaPortion
+		{
+			get
+			{
+				var temp = (Balance / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
+				return temp.Substring(0, temp.IndexOf('.'));
+			}
+		}
 
-        public string Currency
-        {
-            get
-            {
-                if (Network == NetworkType.Mainnet)
-                {
-                    return "₳";
-                }
-                else if (Network == NetworkType.Testnet)
-                {
-                    return "t₳";
-                }
-                return "";
-            }
-        }
-    }
+		public string BalanceLovelacePortion
+		{
+			get
+			{
+				return (Balance % 1000000).ToString("D6");
+			}
+		}
+
+		public string TokenPreservationAdaPortion
+		{
+			get
+			{
+				var temp = (TokenPreservation / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
+				return temp.Substring(0, temp.IndexOf('.'));
+			}
+		}
+
+		public string TokenPreservationLovelacePortion
+		{
+			get
+			{
+				return (TokenPreservation % 1000000).ToString("D6");
+			}
+		}
+
+		public string Currency
+		{
+			get
+			{
+				if (Network == NetworkType.Mainnet)
+				{
+					return "₳";
+				}
+				else if (Network == NetworkType.Testnet)
+				{
+					return "t₳";
+				}
+				return "";
+			}
+		}
+	}
 }
